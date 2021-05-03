ad512b1fac35a714c387e50d6aaf21838e2435e8
Carlos Muñoz Rodriguez <carlosmunozrodriguez@outlook.com>
2021-05-02 22:03:28 -0500

    Algunas observaciones

--- Models/DataContext/ApplicationDbContext.cs
+++ Models/DataContext/ApplicationDbContext.cs
@@ -4,12 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace APITienda.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
+
         public DbSet<Persona> Persona { get; set; }
         public DbSet<Contacto> Contacto { get; set; }
     }
--- Models/Entities/Persona.cs
+++ Models/Entities/Persona.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace APITienda.Models
 {
@@ -10,11 +7,17 @@ namespace APITienda.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string TipoDoc { get; set; }
+
         public string NroDoc { get; set; }
+
         public string Apellido { get; set; }
+
         public string Nombre { get; set; }
+
         public DateTime FechaNacimiento { get; set; }
+
         public string Sexo { get; set; }
     }
 }
